d417cfbe9b0ea1eac9f9ffd57375064f17484a56
blackpysc <ext404@gmail.com>
2024-03-01 16:40:45 +0700

    nothing

---
+++ auth.ts
@@ -0,0 +1,84 @@
+import nextAuth, { NextAuthOptions, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { Adapter } from "next-auth/adapters";
+import prisma from "@/lib/prisma";
+import {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+
+export const config = {
+    pages: {
+      signIn: "/login",
+    },
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      }),
+    ],
+    session: {
+      strategy: "jwt",
+    },
+    callbacks: {
+        async session({session,token}) {
+            if (token) {
+                session.user.id = token.id;
+                session.user.name = token.name;
+                session.user.email = token.email;
+                session.user.image = token.picture;
+                session.user.username = token.username;
+            }
+
+            return session
+        },
+        async jwt({token, user}){
+            const prismaUser = await prisma.user.findFirst({
+              where: {
+                email: token.email
+              }
+            })
+
+            if (!prismaUser) {
+              token.id = user.id
+              return token
+            }
+            if (!prismaUser.username) {
+              await prisma.user.update({
+                where: {
+                  id: prismaUser.id,
+                },
+                data: {
+                  username: prismaUser.name?.split(" ").join("").toLowerCase(),
+                },
+              });
+            }
+
+            return {
+              id: prismaUser.id,
+              name: prismaUser.name,
+              email: prismaUser.email,
+              username: prismaUser.username,
+              picture: prismaUser.image
+            }
+        }
+    },
+} satisfies NextAuthOptions
+
+export default nextAuth(config)
+
+
+
+// gatau ini apaan njirr tapi katanya perlu ini , 
+// !!!!! wajin di pelajari nanti!!!!
+export function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, config);
+}
